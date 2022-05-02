b419eb5824087b704f25a589f5e6d683e591a0c2
Espen Raugstad <espen.raugstad@gmail.com>
2022-05-03 00:39:22 +0200

    Added simple frontend. Unresolved cors issue

--- server.js
+++ server.js
@@ -13,14 +13,27 @@ const config = {
 };
 
 server.use(auth(config));
+server.use(express.static("public"));
+server.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-server.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+server.get("/isLoggedIn", (req, res) => {
+  let isLoggedIn = req.oidc.isAuthenticated();
+
+  try {
+    res.json({ isLoggedIn: isLoggedIn });
+  } catch (err) {
+    console.log(err);
+  }
+
+  /* res
+    .json(req.oidc.isAuthenticated() ? { loggedIn: true } : { loggedIn: false })
+    .end(); */
 });
 
-server.get("/profile", requiresAuth(), (req, res) => {
+server.get("/getProfile", requiresAuth(), (req, res) => {
+  console.log("Getting profile!");
   res.send(JSON.stringify(req.oidc.user));
 });
 
